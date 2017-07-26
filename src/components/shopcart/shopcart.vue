c8a9c8fb13e0631860faac38f2c3a7a551280fab
GM <578160191@qq.com>
2017-07-26 23:19:14 +0800

    add shopcartlist

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="shopcart">
+	<div class="shopcart" @click="toggleshow">
 		<div class="shopcontent">
 			<div class="content-left">
 				<div class="logo-wrap" >
@@ -24,16 +24,40 @@
 				<div class="innerball"></div>
 			</div>
 		</div>
+		<transition name="moveup">
+			<div class="shopcartlist" v-show="listshow">
+			<div class="list-head">
+				<h1 class="listtitle">购物车</h1>
+				<span class="clearcart">清空</span>
+			</div>
+			<div class="list-content">
+				<ul>
+					<li class="listfood" v-for="food in selectFoods">
+						<span class="listname">{{food.name}}</span>
+						<div class="listprice">
+							<span>￥{{food.price*food.count}}</span>
+						</div>
+						<div class="cartcontrol-wrap">
+							<cartcontrol :food="food"></cartcontrol>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+			
+		</transition>
 	</div>
 
 </template>
 
 
 <script type="text/javascript">
+import cartcontrol from '../cartcontrol/cartcontrol'
 	export default{
 		data(){
 			return{
-				balls:[{show:false},{show:false},{show:false},{show:false}]
+				balls:[{show:false},{show:false},{show:false},{show:false}],
+				fold:true
 			}
 		},
 		props:{
@@ -52,6 +76,14 @@
 						count:1
 					}]
 				}		
+			}
+		},
+		methods:{
+			toggleshow(){
+				if(!this.totalCount){
+					return;
+				}
+				this.fold = !this.fold
 			}
 		},
 		computed:{
@@ -78,8 +110,17 @@
  				}else{
  					return '结算';
  				}
+ 			},
+ 			listshow(){
+ 				if(!this.totalCount){
+ 					this.fold =true
+ 					return false
+ 				}
+ 				let show =!this.fold;
+ 				return show;
  			}
-		}
+		},
+		components:{cartcontrol}
 	};
 </script>
 
@@ -201,4 +242,91 @@
 		bottom: 22px;
 		z-index: 200;
 	}
+
+	.shopcartlist{
+		position: absolute;;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index:-1; 
+	}
+	.list-head{
+		height: 40px;
+		line-height: 40px;
+		padding: 0 18px;
+		background-color: #f3f5f7;
+		border-bottom: 1px solid rgba(7,17,27,0.1)
+	}
+	.listtitle{
+		float: left;
+		font-size: 14px;
+		color: rgb(7,17,27);
+	}
+	.clearcart{
+		font-size: 12px;
+		color: rgb(0,160,220);
+		float: right;
+	}
+	.list-content{
+		padding: 0 18px;
+		max-height:217px;
+		overflow: hidden;
+		background-color: #fff;
+	}
+	.listfood{
+		position: relative;
+		padding: 12px 0;
+		box-sizing: border-box;
+		border-bottom: 1px solid rgb(7,17,27,0.1);
+	}
+	.listname{
+		line-height: 24px;
+		font-size: 14px;
+		color: rgb(7,17,27);
+	}
+	.listprice{
+		position: absolute;
+		right: 90px;
+		bottom:12px;
+		line-height: 24px;
+		font-size: 14px;
+		font-weight: 700;
+		color: rgb(240,20,20);
+	}
+	.cartcontrol-wrap{
+		position: absolute;
+		right: 0;
+		bottom: 6px;
+	}
+
+/*	.moveup-enter,.moveup-enter-active {
+		transform: translate3d(0,-100%,0);
+	  	transition: all 0.5s;
+}
+	 .moveup-leave-active,.moveup-leave-active{
+	  
+	  	transform: translate3d(0,0,0);
+		transition: all 0.5s;
+}*/
+.moveup-enter-active,.moveup-leave-active {
+	 transition: all 1s;
+ transform: translate3d(0,-100%,0);
+
+}
+
+.moveup-fade-enter, .moveup-leave-to{
+	transition: all 1s;
+	transform: translate3d(0,0,0);
+		
+
+}
+/*.moveup-leave-active {
+ transform: translate3d(0,0,0);
+		transition: all 0.5s;
+}
+.moveup-enter, .moveup-leave-to{
+ transform: translate3d(0,0,0);
+		transition: all 0.5s;
+}*/
+ 
 </style>
